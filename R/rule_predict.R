96b45aaddd6e26b9faebf7f996dd8d65a71cca99
Henrik Varmer <you@example.com>
2019-04-17 23:29:47 +0200

    updated prediction function

--- R/rule_predict.R
+++ R/rule_predict.R
@@ -1,12 +1,12 @@
 #' Creates recommendations based on input association rules
 #'
-#' This function returns recommendations based on input association rules
+#' This function returns recommendations based on input association rules and a new unseen dataset
 #' @param rule_train Association Rule Mining Training Vector Object
 #' @export
 #' @examples
-#' rule_predict(rule_train)
+#' rule_predict(rules, newdata)
 
-rule_predict <- function(rule_train) {
+rule_predict <- function(rules, newdata) {
   
   all_rules <- test[1]
   lhs_dat   <- test[2]
