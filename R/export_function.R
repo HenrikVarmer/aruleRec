3431df8d30047375820f2707f2b169d35847c226
Henrik Varmer <you@example.com>
2019-04-17 22:48:03 +0200

    added train function

---
+++ R/export_function.R
@@ -0,0 +1,5 @@
+export_function <- function(...) {
+  arg.list      <- list(...)
+  names         <- all.names(match.call())[-1]
+  for (i in seq_along(names)) assign(names[i],arg.list[[i]],.GlobalEnv)
+}
--- R/rule_train.R
+++ R/rule_train.R
@@ -1,5 +1,50 @@
 
 
-rule_train <- function(){
-  
+rule_train <- function(data, 
+                       productkey, 
+                       customerkey, 
+                       support    = 0.01, 
+                       confidence = 0.7, 
+                       minlen     = 2, 
+                       maxlen     = as.integer(length(unique(data$productkey)) + 1)) {
+    
+    customerkey <- enquo(customerkey)
+    productkey  <- enquo(productkey)
+    
+    rules <- rule_flow(data,
+                       !! customerkey,
+                       !! productkey,
+                       support, 
+                       confidence, 
+                       minlen,
+                       maxlen)
+    
+    all_rules <- rules$rules %>% 
+      mutate(lhs = as.character(lhs))
+    
+    lhs_dat <- data %>% 
+      select(!! customerkey, !! productkey) %>%
+      group_by(!! customerkey, !! productkey) %>% 
+      mutate(rownumber= row_number(!! customerkey)) %>% 
+      filter(rownumber == 1) %>% 
+      ungroup %>% 
+      group_by(!! customerkey) %>% 
+      arrange(!! productkey) %>% 
+      mutate(lhs = as.character(paste0(!! productkey, collapse = ","))) %>% 
+      arrange(!! customerkey) %>% 
+      top_n(1, !! productkey) %>% 
+      select(!! customerkey, lhs) %>%
+      as.data.frame()
+    
+    recommendations <- complete_fun(merge(x = lhs_dat, y = all_rules, by = "lhs", all.x = TRUE), "rhs") %>% 
+      arrange(desc(lift)) %>% 
+      filter(lift >= 1) %>% 
+      as.data.frame()
+    
+    rule_train@all_rules <- all_rules
+    rule_train@lhs_dat   <- lhs_dat
+    
+    export_function(rule_train)
+    
+    return(rule_train)
 }
